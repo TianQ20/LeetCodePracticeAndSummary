1b2e09b040f5c9a6ece846280cf83d321a13ed50
Tian97 <tianqiangcao@gmail.com>
2020-02-12 21:39:42 -0500

    add comment

--- FirstRound/src/Array/Non_overlappingIntervals.java
+++ FirstRound/src/Array/Non_overlappingIntervals.java
@@ -11,6 +11,7 @@ public class Non_overlappingIntervals {
         Arrays.sort(intervals, (i1, i2) -> Integer.compare(i1[1], i2[1]));
         int count = 1;
         int end = intervals[0][1];
+        // get the maximum number of non-overlapping intervals
         for (int i = 1; i < intervals.length; i++) {
             if (intervals[i][0] >= end) {
                 end = intervals[i][1];
