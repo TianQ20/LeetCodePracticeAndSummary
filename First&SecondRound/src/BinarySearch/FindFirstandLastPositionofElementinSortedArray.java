93f9794ee22d4f64435636f431adf77b281de618
Tian97 <tianqiangcao@gmail.com>
2020-02-13 19:08:00 -0500

    Find First and Last Position of Element in Sorted Array Two ways of BS

---
+++ First&SecondRound/src/BinarySearch/FindFirstandLastPositionofElementinSortedArray.java
@@ -0,0 +1,75 @@
+package BinarySearch;
+
+
+// use two Binary Search to find the first and last index
+class FindFirstandLastPositionofElementinSortedArray1 {
+    public int[] searchRange(int[] nums, int target) {
+        int[] result = new int[2];
+        result[0] = findFirst(nums, target);
+        result[1] = findLast(nums, target);
+        return result;
+    }
+
+    private int findFirst(int[] nums, int target) {
+        int idx = -1;
+        int start = 0;
+        int end = nums.length - 1;
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (nums[mid] >= target) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+            if (nums[mid] == target) idx = mid;
+        }
+        return idx;
+    }
+
+    private int findLast(int[] nums, int target) {
+        int idx = -1;
+        int start = 0;
+        int end = nums.length - 1;
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (nums[mid] <= target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+            if (nums[mid] == target) idx = mid;
+        }
+        return idx;
+    }
+}
+
+
+// find the first greater or equal index of target and (target + 1)
+class FindFirstandLastPositionofElementinSortedArray2 {
+    public int[] searchRange(int[] A, int target) {
+        int start = firstGreaterEqual(A, target);
+        if (start == A.length || A[start] != target) {
+            return new int[]{-1, -1};
+        }
+        return new int[]{start, firstGreaterEqual(A, target + 1) - 1};
+    }
+
+    //find the first number that is greater than or equal to target.
+    //could return A.length if target is greater than A[A.length-1].
+    //actually this is the same as lower_bound in C++ STL.
+    private static int firstGreaterEqual(int[] A, int target) {
+        int low = 0, high = A.length;
+        while (low < high) {
+            int mid = low + ((high - low) >> 1);
+            //low <= mid < high
+            if (A[mid] < target) {
+                low = mid + 1;
+            } else {
+                //should not be mid-1 when A[mid]==target.
+                //could be mid even if A[mid]>target because mid<high.
+                high = mid;
+            }
+        }
+        return low;
+    }
+}
