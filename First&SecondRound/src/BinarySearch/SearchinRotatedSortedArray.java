f42651780de893f6e6c59c1acc7583544a99f2d5
Tian97 <tianqiangcao@gmail.com>
2020-02-15 21:33:57 -0500

    Search in Rotated Sorted Array BS

---
+++ First&SecondRound/src/BinarySearch/SearchinRotatedSortedArray.java
@@ -0,0 +1,31 @@
+package BinarySearch;
+
+public class SearchinRotatedSortedArray {
+    public int search(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] == target) return mid;
+
+            // in the left side
+            if (nums[mid] >= nums[start]) {
+                if (target < nums[mid] && target >= nums[start]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            }
+
+            // in the right side
+            if (nums[mid] <= nums[end]) {
+                if (target > nums[mid] && target <= nums[end]) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
