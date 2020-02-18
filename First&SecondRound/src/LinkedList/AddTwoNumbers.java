4939554e51e092b937f16b88bd57fdc3cf2c6c23
Tian97 <tianqiangcao@gmail.com>
2020-02-18 14:18:27 -0500

    2. Add Two Numbers LinkedList

---
+++ First&SecondRound/src/LinkedList/AddTwoNumbers.java
@@ -0,0 +1,32 @@
+package LinkedList;
+
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
+
+public class AddTwoNumbers {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummyHead = new ListNode(0);
+        ListNode p = l1, q = l2, curr = dummyHead;
+        int carry = 0;
+        while (p != null || q != null) {
+            int x = (p != null) ? p.val : 0;
+            int y = (q != null) ? q.val : 0;
+            int sum = carry + x + y;
+            carry = sum / 10;
+            curr.next = new ListNode(sum % 10);
+            curr = curr.next;
+            if (p != null) p = p.next;
+            if (q != null) q = q.next;
+        }
+
+        // The sum could have an extra carry of one at the end, we need to append a new ListNode carry.
+        if (carry > 0) {
+            curr.next = new ListNode(carry);
+        }
+        return dummyHead.next;
+    }
+}
