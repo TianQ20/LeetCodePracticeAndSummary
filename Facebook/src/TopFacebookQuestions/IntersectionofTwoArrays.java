ff87f5a71c50b4fb60d4085abea600a8239b2de9
Tian97 <tianqiangcao@gmail.com>
2020-02-20 14:41:18 -0500

    349. Intersection of Two Arrays add two onther solutions

--- Facebook/src/TopFacebookQuestions/IntersectionofTwoArrays.java
+++ Facebook/src/TopFacebookQuestions/IntersectionofTwoArrays.java
@@ -1,11 +1,12 @@
 package TopFacebookQuestions;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
-public class IntersectionofTwoArrays {
+class IntersectionofTwoArrays1 {
 
-    // use two HashSet in O(n)
+    // two hashset in O(n)
 
     public int[] intersection(int[] nums1, int[] nums2) {
         Set<Integer> set = new HashSet<>();
@@ -24,5 +25,72 @@ public class IntersectionofTwoArrays {
             result[i++] = num;
         }
         return result;
+    }
+}
+
+class IntersectionofTwoArrays2 {
+
+    // sort with two pointers and one hashset in O(nlogn)
+
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Set<Integer> set = new HashSet<>();
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        int i = 0;
+        int j = 0;
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] < nums2[j]) {
+                i++;
+            } else if (nums1[i] > nums2[j]) {
+                j++;
+            } else {
+                set.add(nums1[i]);
+                i++;
+                j++;
+            }
+        }
+        int[] result = new int[set.size()];
+        int k = 0;
+        for (Integer num : set) {
+            result[k++] = num;
+        }
+        return result;
+    }
+}
+class IntersectionofTwoArrays3 {
+
+    // binary search in O(nlogn)
+
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Set<Integer> set = new HashSet<>();
+        Arrays.sort(nums2);
+        for (Integer num : nums1) {
+            if (binarySearch(nums2, num)) {
+                set.add(num);
+            }
+        }
+        int i = 0;
+        int[] result = new int[set.size()];
+        for (Integer num : set) {
+            result[i++] = num;
+        }
+        return result;
+    }
+
+    public boolean binarySearch(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] == target) {
+                return true;
+            }
+            if (nums[mid] > target) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return false;
     }
 }
