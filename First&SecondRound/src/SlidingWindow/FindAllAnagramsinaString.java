91e268ab42a284a25169d0053b3573b468888d27
Tian97 <tianqiangcao@gmail.com>
2020-03-14 17:25:05 -0400

    add sliding window template and solution with Find All Anagrams in a String

---
+++ First&SecondRound/src/SlidingWindow/FindAllAnagramsinaString.java
@@ -0,0 +1,50 @@
+package SlidingWindow;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+public class FindAllAnagramsinaString {
+    class Solution {
+        public List<Integer> findAnagrams(String s, String p) {
+            List<Integer> result = new LinkedList<>();
+            if (p.length() > s.length()) {
+                return result;
+            }
+
+            Map<Character, Integer> map = new HashMap<>();
+            for (char c : p.toCharArray()) {
+                map.put(c, map.getOrDefault(c, 0) + 1);
+            }
+
+            int counter = map.size();
+            int begin = 0, end = 0;
+            while (end < s.length()) {
+                char c = s.charAt(end);
+                if (map.containsKey(c)) {
+                    map.put(c, map.get(c) - 1);
+                    if (map.get(c) ==0) {
+                        counter--;
+                    }
+                }
+                end++;
+
+                while (counter == 0) {
+                    char tempc = s.charAt(begin);
+                    if (map.containsKey(tempc)) {
+                        map.put(tempc, map.get(tempc) + 1);
+                        if (map.get(tempc) > 0) {
+                            counter++;
+                        }
+                    }
+                    if (end - begin == p.length()) {
+                        result.add(begin);
+                    }
+                    begin++;
+                }
+            }
+            return result;
+        }
+    }
+}
