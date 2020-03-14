7e607e626740fb51c74890e511b6e457d73b764e
Tian97 <tianqiangcao@gmail.com>
2020-03-14 00:04:37 -0400

    Task Scheduler with greedy

---
+++ First&SecondRound/src/Greedy/TaskScheduler.java
@@ -0,0 +1,26 @@
+package Greedy;
+
+public class TaskScheduler {
+    class Solution {
+        public int leastInterval(char[] tasks, int n) {
+            int[] counter = new int[26];
+            int max = 0;
+            int maxCount = 0;
+            for (char task : tasks) {
+                counter[task - 'A']++;
+                if (max == counter[task - 'A']) {
+                    maxCount++;
+                } else if (max < counter[task - 'A']) {
+                    max = counter[task -'A'];
+                    maxCount = 1;
+                }
+            }
+            int partCount = max - 1;
+            int partLength = n - (maxCount - 1); // may encounter different character has the same frequency
+            int emptySlots = partCount * partLength;
+            int availableTasks = tasks.length - max * maxCount;
+            int idles = Math.max(0, emptySlots - availableTasks);
+            return tasks.length + idles;
+        }
+    }
+}
