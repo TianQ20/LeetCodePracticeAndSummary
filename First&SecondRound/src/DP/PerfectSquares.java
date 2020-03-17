8d694b17ae20ae38c5dc2473fa0fa06fad3e505a
Tian97 <tianqiangcao@gmail.com>
2020-03-17 19:30:03 -0400

    Perfect Squares, regular dp

---
+++ First&SecondRound/src/DP/PerfectSquares.java
@@ -0,0 +1,20 @@
+package DP;
+
+import java.util.Arrays;
+
+public class PerfectSquares {
+    class Solution {
+        public int numSquares(int n) {
+            // dp[n] indicates that the perfect squares count of the given n
+            int[] dp = new int[n + 1];
+            Arrays.fill(dp, Integer.MAX_VALUE);
+            dp[0] = 0;
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j * j <= i; j++) {
+                    dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+                }
+            }
+            return dp[n];
+        }
+    }
+}
