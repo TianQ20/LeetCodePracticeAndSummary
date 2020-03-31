7771bed37c5c71a0e2afa3451661852387fe460c
Tian97 <tianqiangcao@gmail.com>
2020-03-30 22:55:18 -0400

    77. Combinations

---
+++ LeetCodeTopicsReview/src/Search/Combinations.java
@@ -0,0 +1,28 @@
+package Search;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Combinations {
+    class Solution {
+        public List<List<Integer>> combine(int n, int k) {
+            List<List<Integer>> list = new ArrayList<>();
+            List<Integer> curr = new ArrayList<>();
+            DFS(n, k, 1, curr, list);
+            return list;
+        }
+
+        public void DFS(int n, int k, int start, List<Integer> curr, List<List<Integer>> list) {
+            if (k == 0) {
+                list.add(new ArrayList<>(curr));
+                return;
+            }
+
+            for (int i = start; i <= n - k + 1; i++) { // should not continue recursing there won't be enough numbers left until n to fill the needed k slots.
+                curr.add(i);
+                DFS(n, k - 1, i + 1, curr, list);
+                curr.remove(curr.size() - 1);
+            }
+        }
+    }
+}
