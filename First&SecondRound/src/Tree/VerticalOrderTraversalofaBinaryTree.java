c2d0fe468a30d94d1ad2cc9e987afe9dafdad0e0
Tian97 <tianqiangcao@gmail.com>
2020-04-04 16:31:48 -0400

    987. Vertical Order Traversal of a Binary Tree

---
+++ First&SecondRound/src/Tree/VerticalOrderTraversalofaBinaryTree.java
@@ -0,0 +1,58 @@
+package Tree;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.TreeMap;
+
+public class VerticalOrderTraversalofaBinaryTree {
+
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     *     int val;
+     *     TreeNode left;
+     *     TreeNode right;
+     *     TreeNode(int x) { val = x; }
+     * }
+     */
+
+
+    // if the node shows up at a higher level it will be added first
+    // if two nodes are on the same level then they will be sorted
+    // Do not fully sort a vertical column! That will fail test cases.
+
+        
+    class Solution {
+        public List<List<Integer>> verticalTraversal(TreeNode root) {
+            TreeMap<Integer, TreeMap<Integer, PriorityQueue<Integer>>> map = new TreeMap<>();
+            dfs(root, 0, 0, map);
+            List<List<Integer>> list = new ArrayList<>();
+            for (TreeMap<Integer, PriorityQueue<Integer>> ys : map.values()) {
+                list.add(new ArrayList<>());
+                for (PriorityQueue<Integer> nodes : ys.values()) {
+                    while (!nodes.isEmpty()) {
+                        list.get(list.size() - 1).add(nodes.poll());
+                    }
+                }
+            }
+            return list;
+        }
+
+        private void dfs(TreeNode root, int x, int y, TreeMap<Integer, TreeMap<Integer, PriorityQueue<Integer>>> map) {
+            if (root == null) return;
+
+            if (!map.containsKey(x)) {
+                map.put(x, new TreeMap<>());
+            }
+
+            if (!map.get(x).containsKey(y)) {
+                map.get(x).put(y, new PriorityQueue<>());
+            }
+
+            map.get(x).get(y).offer(root.val); // insert
+            dfs(root.left, x - 1, y + 1, map);
+            dfs(root.right, x + 1, y + 1, map);
+        }
+    }
+}
