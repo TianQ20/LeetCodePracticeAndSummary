0542d17ad3a27eece5282b6857e96174798cfe6e
Tian97 <tianqiangcao@gmail.com>
2020-04-06 20:35:24 -0400

    49. Group Anagrams

---
+++ First&SecondRound/src/HashTable/GroupAnagrams.java
@@ -0,0 +1,22 @@
+package HashTable;
+
+import java.util.*;
+
+public class GroupAnagrams {
+    class Solution {
+        public List<List<String>> groupAnagrams(String[] strs) {
+            if (strs.length == 0) return new ArrayList<>();
+            Map<String, List> map = new HashMap<>();
+            for (String s : strs) {
+                char[] c = s.toCharArray();
+                Arrays.sort(c);
+                String key = String.valueOf(c);
+                if (!map.containsKey(key)) {
+                    map.put(key, new ArrayList<>());
+                }
+                map.get(key).add(s);
+            }
+            return new ArrayList(map.values());
+        }
+    }
+}
