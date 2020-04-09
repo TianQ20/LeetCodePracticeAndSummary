cd4936e11f1c2c610386a0f0dfda3c93d07454e2
Tian97 <tianqiangcao@gmail.com>
2020-04-08 22:48:47 -0400

    876. Middle of the Linked List

---
+++ First&SecondRound/src/TwoPointers/MiddleoftheLinkedList.java
@@ -0,0 +1,22 @@
+package TwoPointers;
+
+public class MiddleoftheLinkedList {
+
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+    }
+
+    class Solution {
+        public ListNode middleNode(ListNode head) {
+            ListNode fast = head;
+            ListNode slow = head;
+            while (fast != null && fast.next != null) {
+                fast = fast.next.next;
+                slow = slow.next;
+            }
+            return slow;
+        }
+    }
+}
