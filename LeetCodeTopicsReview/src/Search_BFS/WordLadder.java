3ad4fcf58da3e2911bc007a2e8554f1d10fcde04
Tian97 <tianqiangcao@gmail.com>
2020-04-08 23:06:43 -0400

    127. Word Ladder. Bidirectional BFS.

--- LeetCodeTopicsReview/src/Search_BFS/WordLadder.java
+++ LeetCodeTopicsReview/src/Search_BFS/WordLadder.java
@@ -3,9 +3,10 @@ package Search_BFS;
 import java.util.*;
 
 public class WordLadder {
+
     // One way BFS, looking for goal during the one way search.
 
-    class Solution {
+    class Solution_BFS {
         public int ladderLength(String beginWord, String endWord, List<String> wordList) {
             Set<String> dict = new HashSet<>();
             for (String word : wordList) {
@@ -46,6 +47,59 @@ public class WordLadder {
                         chs[i] = ch;
                     }
                 }
+            }
+            return 0;
+        }
+    }
+
+
+    // Bidirectional BFS. Always update the small set and swap them.
+
+    class Solution {
+        public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+            Set<String> dict = new HashSet<>();
+            for (String word : wordList) {
+                dict.add(word);
+            }
+            if (!dict.contains(endWord)) return 0;
+
+            Set<String> q1 = new HashSet<>();
+            Set<String> q2 = new HashSet<>();
+            q1.add(beginWord);
+            q2.add(endWord);
+
+            int l = beginWord.length();
+            int step = 0;
+
+            while (!q1.isEmpty() && !q2.isEmpty()) {
+                ++step;
+                // swap q1 q2
+                if (q1.size() > q2.size()) {
+                    Set<String> tmp = q1;
+                    q1 = q2;
+                    q2 = tmp;
+                }
+
+                Set<String> q = new HashSet<>();
+
+                for (String w : q1) {
+                    char[] chs = w.toCharArray();
+                    for (int i = 0; i < chs.length; i++) {
+                        char ch = chs[i];
+                        for (char c = 'a'; c <= 'z'; c++) {
+                            chs[i] = c;
+                            String t = new String(chs);
+                            if (q2.contains(t)) {
+                                return step + 1;
+                            }
+                            if (!dict.contains(t)) continue;
+                            dict.remove(t);
+                            q.add(t);
+                        }
+                        chs[i] = ch;
+                    }
+                }
+                q1 = q; // update the small set
             }
             return 0;
         }
