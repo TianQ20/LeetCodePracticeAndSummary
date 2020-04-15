3c76d81ab2c014bb7f36faaaf1f817f203a2aeaa
Tian97 <tianqiangcao@gmail.com>
2020-04-15 18:04:12 -0400

    238. Product of Array Except Self

--- First&SecondRound/src/Array/ProductofArrayExceptSelf.java
+++ First&SecondRound/src/Array/ProductofArrayExceptSelf.java
@@ -18,6 +18,7 @@ public class ProductofArrayExceptSelf {
     public int[] productExceptSelf(int[] nums) {
         int n = nums.length;
         int[] res = new int[n];
+        // Calculate lefts and store in res.
         int left = 1;
         for (int i = 0; i < n; i++) {
             if (i > 0) {
@@ -25,6 +26,7 @@ public class ProductofArrayExceptSelf {
             }
             res[i] = left;
         }
+        // Calculate rights and the product from the end of the array.
         int right = 1;
         for (int i = n - 1; i >= 0; i--) {
             if (i < n - 1) {
