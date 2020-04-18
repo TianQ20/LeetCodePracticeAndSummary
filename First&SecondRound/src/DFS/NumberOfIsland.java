c3a9ccf9d26f4f653eec4e7ac712d1271bd05b89
Tian97 <tianqiangcao@gmail.com>
2020-04-17 23:47:45 -0400

    200. Number of Islands

---
+++ First&SecondRound/src/DFS/NumberOfIsland.java
@@ -0,0 +1,33 @@
+package DFS;
+
+public class NumberOfIsland {
+    class Solution {
+        public int numIslands(char[][] grid) {
+            int res = 0;
+            int m = grid.length;
+            if (m == 0) return 0;
+            int n = grid[0].length;
+
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (grid[i][j] == '1') {
+                        dfs(grid, i, j);
+                        res++;
+                    }
+                }
+            }
+            return res;
+        }
+        // four direction recursion
+        private void dfs(char[][] grid, int i, int j) {
+            if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == '0') {
+                return;
+            }
+            grid[i][j] = '0';
+            dfs(grid, i + 1, j);
+            dfs(grid, i - 1, j);
+            dfs(grid, i, j + 1);
+            dfs(grid, i, j - 1);
+        }
+    }
+}
