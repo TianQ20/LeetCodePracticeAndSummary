0c4984c6c723595569d7c020e0cda01f6283ae59
Tian97 <tianqiangcao@gmail.com>
2020-05-15 08:14:06 -0400

    299. Bulls and Cows. Add hashmap solution.

--- AlgoTogether/src/FirstWeek/BullsandCows.java
+++ AlgoTogether/src/FirstWeek/BullsandCows.java
@@ -1,5 +1,8 @@
 package FirstWeek;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class BullsandCows {
     // use two arrays
     class Solution_1 {
@@ -47,5 +50,31 @@ public class BullsandCows {
  * numbers[secret.charAt(i)-'0'] is negative only if this character appeared in the guess more times then in the secret
  * which means that this character in the secret can be matched with one of the previous characters in the guest.
   */
+
+    // use HashMap
+    class Solution_3 {
+        public String getHint(String secret, String guess) {
+            int count_A = 0, count_B = 0;
+            Map<Character, Integer> map = new HashMap<>();
+            for (char c : secret.toCharArray()) {
+                map.put(c, map.getOrDefault(c, 0) + 1);
+            }
+            for (char c : guess.toCharArray()) {
+                if (map.containsKey(c)) {
+                    if (map.get(c) != 0) {
+                        map.put(c, map.get(c) - 1);
+                        count_B += 1;
+                    }
+                }
+            }
+            for (int i = 0; i < secret.length(); i++) {
+                if (secret.charAt(i) == guess.charAt(i)) {
+                    count_A += 1;
+                    count_B -= 1;
+                }
+            }
+            return "" + count_A + "A" + count_B + "B";
+        }
+    }
 
 }
