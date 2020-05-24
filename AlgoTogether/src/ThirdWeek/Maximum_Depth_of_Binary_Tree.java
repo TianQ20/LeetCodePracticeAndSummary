61e72c072529b9766ee577dcd468c88673485252
Tian97 <tianqiangcao@gmail.com>
2020-05-24 19:43:55 -0400

    104. Maximum Depth of Binary Tree

---
+++ AlgoTogether/src/ThirdWeek/Maximum_Depth_of_Binary_Tree.java
@@ -0,0 +1,15 @@
+package ThirdWeek;
+
+public class Maximum_Depth_of_Binary_Tree {
+    class Solution {
+        public int maxDepth(TreeNode root) {
+            if (root == null) {
+                return 0;
+            }
+
+            int left = maxDepth(root.left);
+            int right = maxDepth(root.right);
+            return Math.max(left, right) + 1;
+        }
+    }
+}
