c0f0bfa22ed6a418d49e0533218e38e2777ab69a
Tian97 <tianqiangcao@gmail.com>
2020-05-26 17:15:36 -0400

    113. Path Sum II

---
+++ AlgoTogether/src/ThirdWeek/Path_SumII.java
@@ -0,0 +1,30 @@
+package ThirdWeek;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Path_SumII {
+    class Solution {
+        public List<List<Integer>> pathSum(TreeNode root, int sum) {
+            List<List<Integer>> res = new ArrayList<>();
+            List<Integer> cur = new ArrayList<>();
+            pathSum(root, sum, cur, res);
+            return res;
+        }
+
+        private void pathSum(TreeNode root, int sum, List<Integer> cur, List<List<Integer>> res) {
+            if (root == null) {
+                return;
+            }
+
+            cur.add(root.val);
+            if (root.left == null && root.right == null && root.val == sum) {
+                res.add(new ArrayList(cur));
+            } else {
+                pathSum(root.left, sum - root.val, cur, res);
+                pathSum(root.right, sum - root.val, cur, res);
+            }
+            cur.remove(cur.size() - 1);
+        }
+    }
+}
